57773684c1e06644deebdccc7d4188da2e577a9c
Bystroushaak <bystrousak@kitakitsune.org>
2012-12-09 02:28:23 +0100

    Added doc strings, few comments.

--- timetracker.py
+++ timetracker.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __name    = "timetracker"
-__version = "0.0.1"
-__date    = "08.12.2012"
+__version = "1.0.0"
+__date    = "09.12.2012"
 __author  = "Bystroushaak"
 __email   = "bystrousak@kitakitsune.org"
 # 
@@ -25,8 +25,6 @@ import pyinotify
 
 
 #= Variables ===================================================================
-watched_dir = "tajemstvi/hesla.txt"
-
 CONF_DIR     = os.path.expanduser("~/.timetracker")
 WATCHLIST_FN = CONF_DIR + "/" + "watchlist.txt"
 WATCHLOG_FN  = CONF_DIR + "/" + "watchlog.txt"
@@ -45,6 +43,8 @@ def version():
 
 
 def readFile(fn):
+	"Just read file and return it as string."
+
 	fh = open(fn, "rt")
 	data = fh.read()
 	fh.close()
@@ -52,22 +52,30 @@ def readFile(fn):
 	return data
 
 def readWatchlist(fn):
+	"Read file and return it as unsorted unique list. Filter blank lines."
+
 	return list(set(filter(lambda x: x.strip() != "", readFile(fn).splitlines())))
 
 
 def addToFile(fn, line):
+	"Append line to the file identified with fn."
+
 	fh = open(fn, "a")
 	fh.write(line.strip() + "\n")
 	fh.close()
 
 
 def saveList(fn, data):
+	"Save list to the file as string."
+
 	fh = open(fn, "wt")
 	fh.write("\n".join(data) + "\n")
 	fh.close()
 
 
 def printWatchlist(wl):
+	"Print numbered list, if blank, show let user know and exit."
+
 	if len(wl) <= 0:
 		writeln("There is nothing on watchlist.")
 		sys.exit(0)
@@ -79,6 +87,8 @@ def printWatchlist(wl):
 
 
 def analyzeLogFiles(watchlist, loglist):
+	"Analyze watchlist and loglist and return stats for each project listed in watchlist."
+
 	data = {}
 	for project in watchlist:
 		data[project] = 0
@@ -86,6 +96,7 @@ def analyzeLogFiles(watchlist, loglist):
 
 	# read saved data from logfile
 	for line in loglist:
+		# skip regular log records and blank lines
 		if not line.startswith("saved") or line.strip() == "":
 			continue
 
@@ -103,6 +114,7 @@ def analyzeLogFiles(watchlist, loglist):
 	# process logfile for file access
 	for project in filter(lambda line: not line.startswith("saved"), watchlist):
 		for line in loglist:
+			# skip saved records and blank lines
 			if line.startswith("saved") or line.strip() == "":
 				continue
 
@@ -110,16 +122,17 @@ def analyzeLogFiles(watchlist, loglist):
 			date = int(line[:10])
 			line = line[11:]
 
+			# count only records which diff more than MIN_TIME_DIFF
 			if line.startswith(project) and abs(date - data[project + "_lt"]) > MIN_TIME_DIFF:
 				data[project] += 1
 				data[project + "_lt"] = date
 
-
 	return data
 
 
-
 class EventHandler(pyinotify.ProcessEvent):
+	"Event based system. Bleh."
+
 	def monitorEvent(self, event):
 		writeln(event.maskname + " " + event.pathname)
 		addToFile(WATCHLOG_FN, str(int(time.time())) + " " + event.pathname)
@@ -195,6 +208,7 @@ if __name__ == '__main__':
 	wl = readWatchlist(WATCHLIST_FN)
 
 
+	# react to user wishes
 	if args.version:
 		writeln(version())
 		sys.exit(0)
@@ -203,8 +217,10 @@ if __name__ == '__main__':
 		printWatchlist(wl)
 
 	elif args.add != "":
+		# get absolute path of file user wants add
 		pp = os.path.abspath(os.path.expanduser(args.add.strip()))
 
+		# check if file exists
 		if not os.path.exists(pp):
 			writeln("Selected path '" + pp + "' doesn't exist!", sys.stderr)
 			sys.exit(2)
@@ -213,23 +229,28 @@ if __name__ == '__main__':
 
 	elif args.remove >= 0 and args.remove < len(wl):
 		del wl[args.remove]
+
 		saveList(WATCHLIST_FN, wl)
+
 		printWatchlist(wl)
 
 	elif args.stats:
+		# analyze logs
 		data = analyzeLogFiles(wl, readFile(WATCHLOG_FN).splitlines())
 		
+		# print results and save it 
 		new_ll = []
 		for project in filter(lambda x: not x.endswith("_lt"), data.keys()):
 			writeln("Aprox. " + str(data[project] * MIN_TIME_DIFF) + "s\t" + project)
 
-			# save results for next time
+			# memorize results for next time
 			new_ll.append("saved " + str(data[project]) + " " + str(data[project + "_lt"]) + " " + project)
 
 		# save results - this saves a lot of time and some diskspace
 		saveList(WATCHLOG_FN, new_ll)
 
 	elif args.daemon:
+		# well, run at background and log access to the projects
 		wm = pyinotify.WatchManager()
 		notifier = pyinotify.Notifier(wm, EventHandler())
 
